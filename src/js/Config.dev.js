c85ae94f787dc16b93acc0d4cbc099b9c024eebb
Raúl Yeguas <neokore@gmail.com>
2017-10-11 11:36:26 +0200

    Set https for API on dev

--- src/js/Config.dev.js
+++ src/js/Config.dev.js
@@ -23,7 +23,7 @@ var App = App || {};
 var baseURL = 'urbo-backend-dev.geographica.gs/api'
 
 App.config = {
-  'api_url' : 'http://' + baseURL,
+  'api_url' : 'https://' + baseURL,
   'ws_url' : 'ws://' + baseURL + '/',
   'map_position':[36.7196718,4.4167761],
   'layout' : 'basetheme',
