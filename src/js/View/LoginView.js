14a0a1c8a31c642d0b1e5ac428133bbc14642933
Raúl Yeguas <neokore@gmail.com>
2017-11-20 10:24:51 +0100

    Fiware Zone Andalucia logos

--- src/js/View/LoginView.js
+++ src/js/View/LoginView.js
@@ -1,20 +1,20 @@
 // Copyright 2017 Telefónica Digital España S.L.
-// 
+//
 // This file is part of UrboCore WWW.
-// 
+//
 // UrboCore WWW is free software: you can redistribute it and/or
 // modify it under the terms of the GNU Affero General Public License as
 // published by the Free Software Foundation, either version 3 of the
 // License, or (at your option) any later version.
-// 
+//
 // UrboCore WWW is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
 // General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Affero General Public License
 // along with UrboCore WWW. If not, see http://www.gnu.org/licenses/.
-// 
+//
 // For those usages not covered by this license please contact with
 // iot_support at tid dot es
 
@@ -31,11 +31,11 @@ App.View.Login = Backbone.View.extend({
     'click input[type="submit"]': 'login'
   },
 
-  onClose: function(){
+  onClose: function () {
     this.stopListening();
   },
 
-  login: function(e){
+  login: function (e) {
     e.preventDefault();
     var _this = this;
 
@@ -48,29 +48,28 @@ App.View.Login = Backbone.View.extend({
     if (!email || !password)
       this.$el.addClass('error');
 
-    App.auth.login(email,md5(password),function(err){
+    App.auth.login(email, md5(password), function (err) {
       if (err) {
         _this.$el.addClass('error');
         _this.$('.loading').remove();
       } else {
-        App.mv().start(function(){
-          App.router.navigate('',{trigger: true});
+        App.mv().start(function () {
+          App.router.navigate('', {trigger: true});
           _this._headerView.render();
         })
       }
     });
   },
 
-  render: function(){
-
+  render: function () {
     var logo;
 
-    switch(App.config.layout){
+    switch (App.config.layout) {
       case 'fiware':
         logo = 'fiwaremaps_logo-negativo.svg';
         break;
       case 'fiware_zone_andalucia':
-        logo = 'fiware_zone_logo_negativo.png';
+        logo = '../verticals/theme-fiware/img/fiware_zone_andalucia_login.png';
         break;
       case 'cedus':
         logo = '../verticals/theme-cedus/img/cedus_logo_login@2x.png';
@@ -79,7 +78,7 @@ App.View.Login = Backbone.View.extend({
         logo = 'telefonica-logo_negativo.svg';
     }
 
-    this.$el.html(this._template({ logo: logo}));
+    this.$el.html(this._template({logo: logo}));
     this.$email = this.$('input[name="email"]');
     this.$password = this.$('input[name="password"]');
 
