9b7c3d0c7d3f143ef4777429e87766c2c7d6026a
Ivan Moreno <ivan@geographica.gs>
2018-01-03 13:48:02 +0100

    change basemap and prepare layer

--- src/js/View/Map/MapboxGLMapView.js
+++ src/js/View/Map/MapboxGLMapView.js
@@ -17,33 +17,67 @@
 // 
 // For those usages not covered by this license please contact with
 // iot_support at tid dot es
-
 'use strict';
-
 App.View.Map.MapboxView = Backbone.View.extend({
 
+  basemaps: {},
+  _currentBasemap: 'dark',
+  _availableBasemaps: ['dark','positron'],
+  _center: [0,0],
+  _zoom: 12,
+  _map: null,
+
   initialize: function(options) {
-    console.log("asdasdas");
+    this._currentBasemap = options.defaultBasemap || 'dark';
+    this._availableBasemaps = options.availableBasemaps || ['dark','positron'];
+    this._center = options.center || [0, 0];
+    this._zoom = options.zoom || 12;
     this.$el[0].id = "map";
+    this._preloadBasemaps();
   },
 
   render: function() {
     setTimeout(()=>{
       mapboxgl.accessToken = 'pk.eyJ1Ijoiam9zbW9yc290IiwiYSI6ImNqYXBvcW9oNjVlaDAyeHIxejdtbmdvbXIifQ.a3H7tK8uHIaXbU7K34Q1RA';
-      var map = new mapboxgl.Map({
+      this._map = new mapboxgl.Map({
         container: this.$el[0],
-        style: 'http://localhost:8085/mapstyles/positron.json'
+        style: `/mapstyles/${this._currentBasemap}.json`,
+        center: this._center,
+        zoom: this._zoom,
       });
     },100)
-  },
-
-  customRender: function() {
-    
   },
 
   onClose: function() {
 
   },
+
+  addLayer: function() {
+
+  },
+
+  changeBasemap: function(name) {
+    this._map.setStyle(this.basemaps[name]);
+    this._currentBasemap = name;
+  },
+
+  _preloadBasemaps: function() {
+    Promise.all(this._availableBasemaps.map(name => {
+      return this._loadBasemap(name);
+    })).then((response) => {
+      Promise.all(response.map(r => r.json())).then(response => {
+        this._availableBasemaps.forEach((bm, i) => {
+          this.basemaps[bm] = response[i];
+        });
+      })
+    });
+  },
+
+  _loadBasemap: function(name) {
+    return fetch(`/mapstyles/${name}.json`);
+  }
+
+
 
 });
 
