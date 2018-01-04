d8314b6471165299dec02829f89c8daf560f27c5
Eduardo Amador Patón <eamadorpaton@gmail.com>
2018-01-04 16:28:42 +0100

    36 - Aquagis theme - Header logo

--- src/js/View/HeaderView.js
+++ src/js/View/HeaderView.js
@@ -1,27 +1,27 @@
 // Copyright 2017 Telefónica Digital España S.L.
-// 
+//
 // This file is part of UrboCore WWW.
-// 
+//
 // UrboCore WWW is free software: you can redistribute it and/or
 // modify it under the terms of the GNU Affero General Public License as
 // published by the Free Software Foundation, either version 3 of the
 // License, or (at your option) any later version.
-// 
+//
 // UrboCore WWW is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
 // General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Affero General Public License
 // along with UrboCore WWW. If not, see http://www.gnu.org/licenses/.
-// 
+//
 // For those usages not covered by this license please contact with
 // iot_support at tid dot es
 
 'use strict';
 
 App.View.HeaderView = Backbone.View.extend({
-  _template: _.template( $('#header_template').html()),
+  _template: _.template( $('#header_template').html() ),
 
   initialize: function(options) {
     this.render();
@@ -52,7 +52,7 @@ App.View.HeaderView = Backbone.View.extend({
   render: function(){
     var logo;
 
-    switch(App.config.layout){
+    switch(App.config.layout) {
       case 'fiware':
         logo = 'fiwaremaps_logo.svg';
         break;
@@ -61,6 +61,9 @@ App.View.HeaderView = Backbone.View.extend({
         break;
       case 'cedus':
         logo = '../verticals/theme-cedus/img/cedus_logo_cab.png';
+        break;
+      case 'aquagis':
+        logo = '../verticals/aq_cons/img/aquagis_logo.svg';
         break;
       default:
         logo = 'telefonica-logo_negativo.svg';
