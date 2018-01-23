63e0244e31ebc7ffae40ee94ed449232cdefd60b
Ivan Moreno <ivan@geographica.gs>
2018-01-23 18:32:59 +0100

    fix popup

--- src/js/View/Map/MapboxGLPopup.js
+++ src/js/View/Map/MapboxGLPopup.js
@@ -42,7 +42,7 @@ App.View.Map.MapboxGLPopup = Backbone.View.extend({
           p.value = clicked.features[0].properties[p.feature];  
         }
         if(p && p.value === 'null') {
-          p = null;
+          p.value = '--';
         }
         if(p && p.value && p.nbf) {
           p.value = p.nbf(p.value);
