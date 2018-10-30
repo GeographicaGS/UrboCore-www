1ccedb6a4e0a984e79e8b3e1bfcaf6fd887c7c3d
ignaciodomingo <ignacio.domingo@geographica.gs>
2018-10-30 12:24:21 +0100

    cleaning up old code

--- src/js/App.js
+++ src/js/App.js
@@ -22,7 +22,6 @@
 
 var ENTER_KEY = 13;
 
-// var availableColors = ['#00d5e7','#f69e37','#fa606f','#3eba94','#3c71db','#9966cc',];
 var availableColors = ["#1f77b4", "#aec7e8", "#ff7f0e", "#ffbb78", "#2ca02c", "#98df8a", "#d62728", "#ff9896", "#9467bd", "#c5b0d5", "#8c564b", "#c49c94", "#e377c2", "#f7b6d2", "#7f7f7f", "#c7c7c7", "#bcbd22", "#dbdb8d", "#17becf", "#9edae5"];
 
 Backbone.View.prototype.close = function(){
