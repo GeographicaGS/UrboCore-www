86638d4f4987e8440eea6687a736d510d3caa994
fjguipen <fjguipen@gmail.com>
2019-03-26 13:09:54 +0100

    added tail ";"

--- src/js/View/Admin/ScopeListView.js
+++ src/js/View/Admin/ScopeListView.js
@@ -162,7 +162,7 @@ App.View.Admin.ScopeList = Backbone.View.extend({
       if (a.name < b.name) return -1;
       if (a.name > b.name) return 1;
       return 0;
-    })
+    });
 
     return orderedScopes;
   }
