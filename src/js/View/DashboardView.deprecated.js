4e500724e7209ea36e1695e7d16963ef6b28bf70
Emilio Fernández <emilio@geographica.gs>
2019-05-02 11:42:59 +0200

    change default date in waste gauge

--- src/js/View/DashboardView.deprecated.js
+++ src/js/View/DashboardView.deprecated.js
@@ -340,7 +340,7 @@ App.View.Dashboard = Backbone.View.extend({
         'className': 'lvigauge',
         'var_id': 'waste.ilv.indicator',
         // 'timeinstant':timeinstant
-        'date': scope === 'guadalajara' ? new Date(2019, 2, 1) : new Date(2016, 7, 1),
+        'date': scope === 'guadalajara' ? new Date(2019, 2, 1) : new Date(2018, 7, 1),
         'url': '/' + scope + '/waste/indicators',
         'extra_info': {
           'unit': '%',
