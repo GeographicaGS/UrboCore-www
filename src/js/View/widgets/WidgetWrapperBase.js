bbac7cb548607e0b78bf6066d0b9e5a135f3875d
fjguipen <fjguipen@gmail.com>
2019-05-16 14:19:44 +0200

    fire qa fixex & filter device list width

--- src/js/View/widgets/WidgetWrapperBase.js
+++ src/js/View/widgets/WidgetWrapperBase.js
@@ -27,11 +27,16 @@ App.View.Widgets.WidgetWrapperBase = App.View.Widgets.Base.extend({
 
   initialize: function(options) {
     options = _.defaults(options, {
-      title: __('Sin título')
+      title: __('Sin título'),
+      stopEventsBbox: false,
     });
 
     // Call to parent class
     App.View.Widgets.Base.prototype.initialize.call(this, options);
+
+    if(options.stopEventsBbox){
+      this.stopListening(App.ctx, 'change:bbox')
+    }
 
     // Skip more code if widget is not allowed
     if(!this.hasPermissions()) return;
