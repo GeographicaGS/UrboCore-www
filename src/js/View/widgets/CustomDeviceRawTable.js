4193416eebf414c8d545ae2cc721b9beadfd7d13
Antonio Irlandés García <limtrack@gmail.com>
2019-06-19 17:37:07 +0200

    Fixing last changes to WidgetMultiVariableChart and Raw Table

--- src/js/View/widgets/CustomDeviceRawTable.js
+++ src/js/View/widgets/CustomDeviceRawTable.js
@@ -1,20 +1,20 @@
 // Copyright 2017 Telefónica Digital España S.L.
-// 
+//
 // This file is part of UrboCore WWW.
-// 
+//
 // UrboCore WWW is free software: you can redistribute it and/or
 // modify it under the terms of the GNU Affero General Public License as
 // published by the Free Software Foundation, either version 3 of the
 // License, or (at your option) any later version.
-// 
+//
 // UrboCore WWW is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
 // General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Affero General Public License
 // along with UrboCore WWW. If not, see http://www.gnu.org/licenses/.
-// 
+//
 // For those usages not covered by this license please contact with
 // iot_support at tid dot es
 
@@ -162,12 +162,7 @@ App.View.Widgets.CustomDeviceRawTable = App.View.Widgets.Base.extend({
   },
 
   dateFn: function (value) {
-    var dateTime = value.split('T') 
-    var date = dateTime[0].split('-')
-    
-    return date.reverse().join('-')
-      + ' '
-      + dateTime[1].slice(0, 5);
+    return App.formatDateTime(value);
   },
 
   getVariableUnits: function (id) {
