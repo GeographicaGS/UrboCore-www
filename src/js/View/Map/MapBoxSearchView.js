a3b116850818c2b90edbe27581733e3af31d030e
fjguipen <fjguipen@gmail.com>
2019-06-24 11:38:26 +0200

    added map filtering support

--- src/js/View/Map/MapBoxSearchView.js
+++ src/js/View/Map/MapBoxSearchView.js
@@ -49,8 +49,8 @@ App.View.MapBoxSearch = App.View.MapSearch.extend({
     this.$('#search_map').addClass('searching');
     this.$('input[type=text]').val($(e.currentTarget).text());
     this.$('ul').removeClass('active');
-    var elem = this._collection.findWhere({ 'element_id': $(e.currentTarget).attr('element_id') });
-    var bbox = elem.get('bbox');
+    var elem = this._collection.findWhere({ 'id_entity': $(e.currentTarget).attr('element_id') });
+    var bbox = elem.get('geometry');
     var maxZoom = 18;
     if (elem.get('type') == 'device') {
       maxZoom = 19;
@@ -59,7 +59,7 @@ App.View.MapBoxSearch = App.View.MapSearch.extend({
     this._marker.setLngLat([bbox[0], bbox[1]])
       .addTo(this._map);
 
-    this._map.fitBounds([[bbox[0], bbox[1]], [bbox[2], bbox[3]]], { maxZoom: maxZoom });
+    this._map.fitBounds([[bbox[0], bbox[1]], [bbox[0], bbox[1]]], { maxZoom: maxZoom });
   },
 
   _updateTerm: _.debounce(function (e) {
