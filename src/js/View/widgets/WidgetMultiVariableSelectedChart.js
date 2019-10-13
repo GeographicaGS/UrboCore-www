89826fdf34d0655517b36656318ef401cf55e4e4
Antonio Irlandés García <limtrack@gmail.com>
2019-10-13 11:29:57 +0200

    WIP

---
+++ src/js/View/widgets/WidgetMultiVariableSelectedChart.js
@@ -0,0 +1,65 @@
+// Copyright 2017 Telefónica Digital España S.L.
+//
+// This file is part of UrboCore WWW.
+//
+// UrboCore WWW is free software: you can redistribute it and/or
+// modify it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// UrboCore WWW is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+// General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with UrboCore WWW. If not, see http://www.gnu.org/licenses/.
+//
+// For those usages not covered by this license please contact with
+// iot_support at tid dot es
+
+'use strict';
+
+App.View.Widgets.MultiVariableSelectedChart = App.View.Widgets.MultiVariableChart.extend({
+
+  // Templates
+  viewTemplate: _.template($('#widgets-widget_multiVariable_selected_chart').html()),
+
+  initialize: function (options) {
+    // Set params new options
+    options = _.defaults(options || {}, {
+      selectedVariable: null,
+      selectableVariables: [],
+    });
+
+    // Set permissions over differents variables of entity
+    options.selectableVariables = _.filter(options.selectableVariables, function (variable) {
+      return App.mv().validateInMetadata({ variables: [variable.id] });
+    });
+
+    App.View.Widgets.MultiVariableChart.prototype.initialize.call(this, options);
+  },
+
+  events: _.extend({
+    'click .popup_widget.variableSelector .varsel li': 'onChangeVariable'
+  }, App.View.Widgets.MultiVariableChart.prototype.events),
+
+  /**
+   * When change the value of selector (title)
+   *
+   * @param {Object} e - triggered event
+   */
+  onChangeVariable: function (e) {
+    debugger;
+    // var $target = $(e.currentTarget);
+    // this.model.set('currentVariable', $target.data('varid'));
+
+    // this.$('.popup_widget.variableSelector > span').html($target.html());
+    // this.$('.popup_widget.variableSelector .selected').removeClass('selected');
+    // $target.addClass('selected');
+
+    // this._drawContent();
+    // this.refresh();
+  },
+
+});
